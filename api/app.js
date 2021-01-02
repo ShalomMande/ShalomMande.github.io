aeb90dacdd882fe1af49cf6c8386fec5873150b7
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-02 13:53:02 -0500

    install esndgrid _ instalise express

---
+++ api/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const sendGrid = require('@sendGrid/mail');
+
+const app = express()
+
+
+
+app.listen(3001,  '0.0.0')
