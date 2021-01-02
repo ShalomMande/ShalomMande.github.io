0e3d50951739be367ff46cad6c76139cccfd350b
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-01 20:30:12 -0500

    add coma

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
