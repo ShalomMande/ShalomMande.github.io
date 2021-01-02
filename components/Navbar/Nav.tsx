c8c41e048abaaab62ec8946b565d23e07e82bd6e
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-01 21:29:01 -0500

    install prettier

--- components/Navbar/Nav.tsx
+++ components/Navbar/Nav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import { FontAwesomeIcon, sun, moon } from "@fortawesome/react-fontawesome"
+import '../styles'
 
 const links = [
   {
