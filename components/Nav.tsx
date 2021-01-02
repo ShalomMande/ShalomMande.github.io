e7159548dc7402d6e42f709be2f9be5a802b40a5
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-01 20:29:55 -0500

    install font awesome and add localStorage to check if darkmode == true or null

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,49 @@
-
 import React from 'react';
+import { useState useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default function Navbar() {
-  return(
-    <>
-      
-    </>
-    
-  )
+
+const links = [
+  {
+    href: "#home",
+    text: "Home"
+  },
+  {
+    href: "#contact",
+    text: "Contact"
+  },
+  {
+    href: "#projects",
+    text: "Projects"
+  },
+]
+
+const Navbar = () => {
+  const [active, setActive] = useState(false)
+
+  let darkMode
+
+  function handleBurderClick(){
+    setActive(prevState => !prevState)
+  }
+
+  setMode()
+
+  function setMode() {
+    if (typeof window !== `undefined`) {
+      if (localStorage.getItem("dark") == "true") {
+        darkMode = true
+      } else{
+        darkMode = false
+      }
+    }
+  }
+
+
+
+
+
+
 }
+
+export default Navbar
