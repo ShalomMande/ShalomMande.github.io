2abfa644d264994dc46b8662822b174503c6833f
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-01 20:37:07 -0500

    loop around dark elemnt and toggle class

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -39,6 +39,25 @@ const Navbar = () => {
     }
   }
 
+  function handleModeToggle() {
+    if (darkMode) {
+      const darkenElements =  document.getElementsByClassName("dark-toggle")
+      const whitenElements = document.getElementsByClassName("white-toggle")
+      const dark2 = document.getElementsByClassName("dark2-toggle")
+
+      for (var i = 0; i < darkenElements.length; i++) {
+        darkenElements[i].classList.toggle("black-bg")
+      }
+      for (var i = 0; i < whitenElements.length; i++) {
+        whitenElements[i].classList.toggle("white")
+      }
+      for (var i = 0; i < dark2.length; i++) {
+        dark2[i].classList.toggle("light-black-bg")
+      }
+
+    }
+  }
+
 
 
 
