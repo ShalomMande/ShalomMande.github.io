6bbcba6f7901a45a70be65870f881ea3f8315611
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-02 13:55:06 -0500

    access headers + geet post put patch delete

--- api/app.js
+++ api/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const sendGrid = require('@sendGrid/mail');
 
 const app = express()
 
+
+app.use(cors());
+
+app.use(req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
 
 
 app.listen(3001,  '0.0.0')
